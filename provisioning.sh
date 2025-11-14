40b4b7054c453763383dd5dff3c010b646ec7dd2
Ankit Bhattarai <67973902+ankit-bhattarai@users.noreply.github.com>
2025-11-14 17:07:17 +0000

    Enhance provisioning script with additional installations
    Added installation of additional dependencies including CUDA, cuDNN, and various Python packages.

--- provisioning.sh
+++ provisioning.sh
@@ -12,8 +12,17 @@ sudo apt-get update && sudo apt-get install -y git
 
 . /venv/main/bin/activate
 
-echo "Installing Python 3.11"
+echo "Installing Python 3.11 and other dependencies"
 conda install python=3.11 -y
+
+mamba install cuda==12.8 cudnn cuda-nvcc cmake -y
+mamba install xorg-xorgproto -y
+ 
+pip install "jax[cuda12_local]<0.6.0" mujoco-mjx mujoco brax
+mamba install ipykernel -y
+conda install conda-forge::ffmpeg -y
+pip install playground mediapy hydra-core wandb
+
 echo "Installation complete"
 
 # Clone your repository
